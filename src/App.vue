37f0988a463a6772f981c9dab9027afbb0356353
Emanuel Sanchez <ejsanch07@gmail.com>
2025-02-11 23:45:46 -0600

    Finished addtransaction component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
   import IncomeExpenses from './components/IncomeExpenses.vue';
+  import AddTransaction from './components/AddTransaction.vue';
+
   import {ref, computed} from 'vue'
+
 
   const transactions = ref([
     {id: 1, text: 'Paycheck', amount: 700.00},
@@ -42,6 +45,7 @@
   <div class="container">
     <Balance :total="sum"></Balance>
     <IncomeExpenses :income="moneyIn" :expense="moneyOut"></IncomeExpenses>
+    <AddTransaction></AddTransaction>
   </div>
 
 </template>
