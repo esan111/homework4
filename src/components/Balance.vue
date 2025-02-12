0b934288e1bb0d731c6a91aab015b5943ef93415
Emanuel Sanchez <ejsanch07@gmail.com>
2025-02-11 22:57:13 -0600

    finished Balance

---
+++ src/components/Balance.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        total: {
+            type: Number,
+            require: true,
+        }
+    })
+
+
+</script>
+
+
+<template>
+    <h2>Your Balance</h2>
+    <h1 id="balance">{{ total }}</h1>
+
+
+</template>
